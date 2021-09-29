6547666e84e54e6c7af121b94eed0dc42e59d251
fahadalduaij <fahad_osama@hotmail.com>
2021-09-29 21:36:48 +0300

    Done parts 1,2,3,4

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ let categories = [];
 tasks = [
   { id: 0, title: "Game of thrones", category: "Movies", done: false },
   { id: 1, title: "Toy Story 4", category: "Movies", done: false },
+  { id: 2, title: "Ahmad", category: "Groceries", done: false },
 ];
 
 categories = ["Movies", "Groceries"];
@@ -18,28 +19,45 @@ renderCategories(categories, CATEGORY_FILTER);
 renderTasks(tasks, "tasks-list");
 
 function taskChecked(taskId, checked) {
-  // implement the delete task.
-  // You are given the task id
-  console.log(`${checked ? "" : "UN"}CHECKED TASK`, taskId);
+	// implement the delete task.
+	// You are given the task id
+	// console.log(`${checked ? "" : "UN"}CHECKED TASK`, taskId);
+  return checked ? true : false
 }
 
 function addTask() {
-  const selectedCategory = getSelectedCategoryById(CATEGORY_SELECTOR);
-  const taskTitle = getNewTaskText();
-  // continue the code here
-  alert(`Category: ${selectedCategory} | Task: ${taskTitle}`);
+	const selectedCategory = getSelectedCategoryById(CATEGORY_SELECTOR);
+	const taskTitle = getNewTaskText();
+	const idNumber = tasks.length++;
+	const task = {
+		id: idNumber,
+		title: taskTitle,
+		category: selectedCategory,
+		done: false,
+	};
+
+	tasks.push(task);
+	categories.push(task.category);
+	renderTasks(tasks, "tasks-list");
 }
 
 function addCategory() {
-  const newCategory = getNewCategoryText();
-  // continue the code here
-  alert(`New category was added: ${newCategory}`);
+	const newCategory = getNewCategoryText();
+
+	categories.push(newCategory);
+	renderCategories(categories, "categories-list");
+	renderCategories(categories, "categories-list-filter");
+	// add style for the alert
+	alert(`New category was added: ${newCategory}`);
 }
 
 function filterTasks() {
-  const selectedCategory = getSelectedCategoryById(CATEGORY_FILTER);
-  const done = getFilteredDone();
-  // continue the code here
-  // REMOVE ME: sample alert
-  alert(`Category: ${selectedCategory} | done: ${done}`);
+	const selectedCategory = getSelectedCategoryById(CATEGORY_FILTER);
+	const done = getFilteredDone();
+
+	const filters = tasks.filter((fil) => fil.category === selectedCategory);
+  
+	renderTasks(filters, "tasks-list");
+	// REMOVE ME: sample alert
+	// alert(`Category: ${selectedCategory} | done: ${done}`);
 }
